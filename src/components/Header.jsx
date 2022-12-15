cda3548d4337c9844a968d7ae190a9bb64a741ab
Michelle David <rachaelholy@gmail.com>
2022-12-15 02:23:59 +0100

    set up dev environment, created header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Header(){
+    return (
+        <>
+            <img src="images/donatewater-logo.jpg" alt="donate water logo"/>
+            <h1 className="text-blue-400">Hey you</h1>
+        </>
+    )
+}
