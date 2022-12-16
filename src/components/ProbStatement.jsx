45eebc150839d684fb73b7a2a80370134ec410c1
Michelle David <rachaelholy@gmail.com>
2022-12-16 22:47:40 +0100

    Updated component style

--- src/components/ProbStatement.jsx
+++ src/components/ProbStatement.jsx
@@ -1,13 +1,15 @@
 export default function ProbStatement(){
     return (
         <>
+         <div className="flex items-center justify-center mt-4">
               <p 
-                  className="font-normal text-sm leading-[140px] w-80" 
+                  className="font-normal text-sm w-80" 
                   id="problem-statement">
                    Over 70% of households in rural communities do not have access to improved water supply.
                     They rely solely on self-water supply (free source) such as rivers, perennial streams, 
                     water ponds and unprotected wells which is susceptible to water borne diseases.
                 </p>
+                </div>
         </>
     )
 }
