1f5b47b908a5ef8bc952c3461d10eca25e8c6855
Michelle David <rachaelholy@gmail.com>
2022-12-20 18:37:01 +0100

    added logo for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,17 @@
 
 export default function Footer(){
     return (
-        <>
-        
+        <footer className="">
+       <div>
+             <span>C</span>
+            <span>2022</span>
+       </div>
+       <div>
+            <img src="images/unicef-img.jpg" alt="unicef logo"/>
+            <img src="images/un-img.jpg" alt="UN logo"/>
+            <img src="images/waid-img.jpg" alt="WAID logo"/>
+       </div>
           
-        </>
+        </footer>
     )
 }
