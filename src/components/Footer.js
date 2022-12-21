c6c1d50619e2581a13a20aa7f22a3474a9b6de37
Michelle David <rachaelholy@gmail.com>
2022-12-21 12:17:45 +0100

    flex footer items

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,12 @@
 
 export default function Footer(){
     return (
-        <footer className="">
-       <div>
+        <footer className="flex flex-row justify-between">
+       <div className="justify-self-start text-white font-bold text-lg ">
              <span>C</span>
             <span>2022</span>
        </div>
-       <div>
+       <div className="justify-self-end flex">
             <img src="images/unicef-img.jpg" alt="unicef logo"/>
             <img src="images/un-img.jpg" alt="UN logo"/>
             <img src="images/waid-img.jpg" alt="WAID logo"/>
