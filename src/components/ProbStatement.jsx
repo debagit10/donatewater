c2d37e2c02143d094ba3f81e2a1369a343fb761b
Michelle David <rachaelholy@gmail.com>
2022-12-23 16:26:55 +0100

    Added mision and vision statement

--- src/components/ProbStatement.jsx
+++ src/components/ProbStatement.jsx
@@ -8,11 +8,34 @@ export default function ProbStatement(){
                    Over 70% of households in rural communities do not have access to improved water supply.
                     They rely solely on self-water supply (free source) such as rivers, perennial streams, 
                     water ponds and unprotected wells which is susceptible to water borne diseases.
-                </p>
+                
                 <small className="self-end mb-7 md:items-center">
                    <a href="#">SOURCE: journal of water resource and protection</a>
-                   </small>
+                </small>
+                </p>
+                <div>
+                    <h3>
+                        vision
+                    </h3>
+                    <p>
+                    To ensure that clean water is accessible in every rural community in Nigeria,
+                     we aim to eliminate the need for people, especially women and children, to travel
+                      long distances in search of water and bear the burden of surviving the impacts of climate change in Nigeria and West Africa.
+                    </p>
                 </div>
+                <div>
+                    <h3>
+                       Mission
+
+                    </h3>
+                    <p>
+                    Our goal is to bridge the gap between communities, individuals, 
+                    and government and local and international agencies in order to improve access to water resources.
+                     By using data, statistics, and human and citizen science, we hope to help excluded populations overcome the impacts and effects of climate change and gender exclusion. 
+                     We will focus on tailoring water projects to the communities that are most in need and work towards a future where everyone has equal access to clean water.
+                    </p>
+                </div>
+            </div>
         </>
     )
 }
