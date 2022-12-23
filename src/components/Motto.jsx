dc072f305f7ef30c997d35514ab7452492914798
Michelle David <rachaelholy@gmail.com>
2022-12-23 01:10:07 +0100

    changed the width for medium screen

--- src/components/Motto.jsx
+++ src/components/Motto.jsx
@@ -3,7 +3,7 @@ export default function Motto(){
         <>
             <div className="relative flex-1 flex-wrap flex md:flex-col items-center ">
                 <img 
-                  className="sm:w-[380px] h-[208px] justify-center md:w-screen md:h-auto"
+                  className="sm:w-[380px] h-[208px] justify-center md:w-screen md:mx-0 md:h-1/2"
                   src="images/front-img.png" alt="main-image-for-motto"
 
                   />
