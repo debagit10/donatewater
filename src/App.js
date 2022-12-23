108a3ca1f8e4685b16e81dfa963fbe84c3bd0269
Michelle David <rachaelholy@gmail.com>
2022-12-23 22:20:46 +0100

    Created NavBar component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Motto from './components/Motto';
 import ProbStatement from './components/ProbStatement'
 import SlidePage from './components/SlidePage'
 import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
 export default function App() {
   return (
     <div className="p-5 flex flex-col md:justify-center md:items-center">
+       <NavBar />
        <Header />
        <Motto />
        <ProbStatement />
--- src/components/ProbStatement.jsx
+++ src/components/ProbStatement.jsx
@@ -14,25 +14,20 @@ export default function ProbStatement(){
                 </small>
                 </p>
                 <div>
-                    <h3>
-                        vision
-                    </h3>
+                    <h3>vision</h3>
                     <p>
-                    To ensure that clean water is accessible in every rural community in Nigeria,
-                     we aim to eliminate the need for people, especially women and children, to travel
-                      long distances in search of water and bear the burden of surviving the impacts of climate change in Nigeria and West Africa.
+                        To ensure that clean water is accessible in every rural community in Nigeria,
+                        we aim to eliminate the need for people, especially women and children, to travel
+                        long distances in search of water and bear the burden of surviving the impacts of climate change in Nigeria and West Africa.
                     </p>
                 </div>
                 <div>
-                    <h3>
-                       Mission
-
-                    </h3>
+                    <h3>Mission</h3>
                     <p>
-                    Our goal is to bridge the gap between communities, individuals, 
-                    and government and local and international agencies in order to improve access to water resources.
-                     By using data, statistics, and human and citizen science, we hope to help excluded populations overcome the impacts and effects of climate change and gender exclusion. 
-                     We will focus on tailoring water projects to the communities that are most in need and work towards a future where everyone has equal access to clean water.
+                        Our goal is to bridge the gap between communities, individuals, 
+                        and government and local and international agencies in order to improve access to water resources.
+                        By using data, statistics, and human and citizen science, we hope to help excluded populations overcome the impacts and effects of climate change and gender exclusion. 
+                        We will focus on tailoring water projects to the communities that are most in need and work towards a future where everyone has equal access to clean water.
                     </p>
                 </div>
             </div>
