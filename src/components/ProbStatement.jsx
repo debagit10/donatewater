21a61ff4ca50c9e1034411054dc0f0540c6f4cc7
Michelle David <rachaelholy@gmail.com>
2022-12-24 02:39:53 +0100

    Aligned text, added google font

--- src/components/ProbStatement.jsx
+++ src/components/ProbStatement.jsx
@@ -3,7 +3,7 @@ export default function ProbStatement(){
         <>
          <div className="flex md:flex-row sm:flex-col flex-wrap justify-center md:items-center mb-7 mt-7">
               <p 
-                  className="font-normal text-sm w-80 md:text-md m-4" 
+                  className="font-normal text-sm w-80 md:text-md m-5" 
                   id="problem-statement">
                    Over 70% of households in rural communities do not have access to improved water supply.
                     They rely solely on self-water supply (free source) such as rivers, perennial streams, 
@@ -13,17 +13,17 @@ export default function ProbStatement(){
                    <a href="#">SOURCE: journal of water resource and protection</a>
                 </small>
                 </p>
-                <div className="w-1/4">
-                    <h3 className="font-bold">vision</h3>
-                    <p className="m-4">
+                <div className="w-1/4 mt-5 m-4">
+                    <h3 className="font-bold text-lg">Vision</h3>
+                    <p className="mt-4">
                         To ensure that clean water is accessible in every rural community in Nigeria,
                         we aim to eliminate the need for people, especially women and children, to travel
                         long distances in search of water and bear the burden of surviving the impacts of climate change in Nigeria and West Africa.
                     </p>
                 </div>
-                <div className="w-1/4">
-                    <h3 className="font-bold">Mission</h3>
-                    <p className="m-4">
+                <div className="w-1/3 mt-5 m-4">
+                    <h3 className="font-bold mt-12 text-lg">Mission</h3>
+                    <p className="mt-4 ">
                         Our goal is to bridge the gap between communities, individuals, 
                         and government and local and international agencies in order to improve access to water resources.
                         By using data, statistics, and human and citizen science, we hope to help excluded populations overcome the impacts and effects of climate change and gender exclusion. 
